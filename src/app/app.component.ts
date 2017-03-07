e3a982c8d7e335864178a5fa4f025b68815fc316
Gabriel Muller <gabriel.muller.12@gmail.com>
2017-03-07 10:37:21 +0000

    header and card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   template: `
-    <xapi-iban></xapi-iban>
+    <md-toolbar color="primary">
+      <span>Santander</span>
+      <span class="title">Experience API Demo</span>
+    </md-toolbar>
+
+    <md-card>
+      <md-card-title>Xapi Iban</md-card-title>
+      <xapi-iban></xapi-iban>
+    </md-card>
+
   `
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { XapiIbanModule } from 'xapi-iban';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    XapiIbanModule
+    XapiIbanModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
